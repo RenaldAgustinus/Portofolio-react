0d7ca217effdda0b9e7b15ef49d406a14c0cd438
Renald Agustinus <renaldagustinus41@gmail.com>
2026-04-12 17:45:21 +0700

    menambahkan navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
   return (
-    <></>
-  //  <h1 className="text-3xl font-bold text-red-500">Hello world</h1>
+    <>
+    <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde repellendus repudiandae, 
+      voluptatem fugiat ad culpa provident molestiae modi necessitatibus sit dolores alias
+      tenetur recusandae nemo porro quidem nihil odio ipsam eveniet at consectetur laudantium id in ut.
+      Officia magnam sapiente ipsum nisi ut dolore expedita commodi similique id animi, 
+      illo atque nostrum sunt tenetur veniam ex debitis adipisci alias cum deleniti earum qui. 
+      Assumenda tempore, aperiam odit sit rerum quae placeat soluta exercitationem laudantium quo 
+      ratione itaque quasi eum doloribus non fugiat impedit deserunt, dicta, molestiae distinctio! 
+      Debitis ad corrupti facilis perferendis, distinctio harum optio quia error, 
+      reprehenderit eos asperiores saepe nam veniam odit? Iure totam voluptatibus obcaecati in 
+      sit pariatur autem rerum alias? Corrupti aperiam necessitatibus est sunt. Iure, corrupti mollitia 
+      autem expedita sed earum saepe quod error natus praesentium dolorem et modi labore dolore non! 
+      Consequatur a quasi rerum error fuga commodi officia. Incidunt saepe reprehenderit asperiores accusamus! 
+      Saepe eius blanditiis dignissimos beatae deleniti, praesentium magni aperiam adipisci nulla voluptatum necessitatibus aliquam corporis id soluta pariatur!
+       Laboriosam neque dignissimos accusantium perferendis, repudiandae saepe aperiam repellendus ratione modi sunt hic natus est eius libero blanditiis.
+        Ipsa, eveniet, rem, recusandae quia excepturi optio provident consectetur accusantium saepe ut placeat quasi.</h1>
+    </>
   )
 }
 

f638cf2745a680899e4553dd16e0769db1bb71fc
Renald Agustinus <renaldagustinus41@gmail.com>
2026-04-14 17:20:53 +0700

    menambahkan action form submit

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ function App() {
         <div className=""kontak mt-32 p-10>
             <h1 className="text-4xl mb-2 font-bold text-center">Kontak</h1>
             <p className="text-base/loose text-center mb-10 opacity-50">Mari Terhubung dengan saya</p>
-            <form action="" className="bg-zinc-800 p-10 w-fit mx-auto rounded-md" autoComplete="off">
+            <form action="https://formsubmit.co/renaldagustinus41@gmail.com" method="POST" className="bg-zinc-800 p-10 w-fit mx-auto rounded-md" autoComplete="off">
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col gap-2">
                   <label className="font-semibold">Nama Lengkap</label>
@@ -126,7 +126,7 @@ function App() {
                 </div>
                 <div className="flex flex-col gap-2">
                   <label htmlFor="pesan" className="font-semibold" >Pesan</label>
-                  <textarea name="pesan" id="pesan" cols="45" rows="7" className="border border-zinc-500 p-2 rounded-md" required>Pesan ...</textarea>
+                  <textarea name="pesan" id="pesan" cols="45" rows="7" className="border border-zinc-500 p-2 rounded-md" required placeholder="Pesan ..."></textarea>
                 </div>
                 <div className="text-center">
                   <button  type="submit" className="bg-violet-700 py-3 rounded-lg w-full cursor-pointer text-center font-bold hover:bg-violet-600 transition-colors shadow-md">Kirim Pesan</button>
